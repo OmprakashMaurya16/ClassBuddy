f2ee3abecb8ac26b95ee38c1656ab27e2fa5bcc6
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2026-03-12 14:45:17 +0530

    Global error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const sendResponse = require("./utils/response.helper.js");
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -9,4 +22,13 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use((err, req, res, next) => {
+  if (err.name === "ApiError") {
+    return sendResponse(res, err.statusCode, err.message);
+  }
+
+  console.error(err.message);
+
+  return sendResponse(res, 500, "Internal Server Error");
+});
 module.exports = app;
